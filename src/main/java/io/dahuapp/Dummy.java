985b8271e004b2c1f125829003fb397dc7e9112f
Rémi BARRAQUAND <dev@remibarraquand.com>
2013-05-22 22:43:47 +0200

    using modal utility window

--- src/main/java/io/dahuapp/Dummy.java
+++ src/main/java/io/dahuapp/Dummy.java
@@ -7,13 +7,11 @@ package io.dahuapp;
 import com.sun.glass.ui.Pixels;
 import com.sun.glass.ui.Screen;
 import java.awt.AWTException;
-import java.awt.Dimension;
 import java.awt.DisplayMode;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
 import java.awt.Robot;
-import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -22,124 +20,102 @@ import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javax.imageio.ImageIO;
-import javax.swing.SwingUtilities;
 
 /**
  *
  * @author barraq
  */
 public class Dummy extends Application {
-    
+
     static int numscreen = 1;
-    
+    static final String NOM_FILE = "screenshot/";
+    static final String NOM_IMAGE = "image"; 			// A CHANGER
+    static final String EXT = "png";
+
     @Override
-    public void start(Stage primaryStage) {
+    public void start(final Stage primaryStage) {
         final StackPane root = new StackPane();
-        
-        final Button btn = new Button();
+
         final ImageView iv = new ImageView();
         iv.fitWidthProperty().bind(root.widthProperty());
         iv.fitHeightProperty().bind(root.heightProperty());
-        
-        btn.setText("Take a screenshot");
-        //btn.setOnAction(new EventHandler<ActionEvent>() {
-          //  @Override
-            //public void handle(ActionEvent event) {
-              //  SwingUtilities.invokeLater(new Runnable() {
-                //    @Override
-                  //  public void run() {
-                    //    takeScreenshotWithAWT(iv);
-                   // }
-               // });
-           // }
-       // });
-        
 
-        Scene scene = new Scene(root, 300, 250);
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
+        final Scene scene = new Scene(root, 300, 250);
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
                 try {
-                       takeScreenshotWithAWT();
-                    } catch (IOException ex) {
-                        Logger.getLogger(Dummy.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+                    takeScreenshotWithAWT(iv);
+                } catch (IOException ex) {
+                    Logger.getLogger(Dummy.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
-       });
-
-        //root.getChildren().add(btn);
+        });
         root.getChildren().add(iv);
-        
-        primaryStage.setTitle("JavaFXGradle dummy screenshot");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+
+        Stage widget = new Stage();
+        widget.setTitle("JavaFXGradle dummy screenshot Widget");
+        widget.initStyle(StageStyle.UTILITY);
+        widget.initModality(Modality.APPLICATION_MODAL);
+        widget.setScene(scene);
+        widget.show();
     }
-    
-    public static void takeScreenshotWithAWT() throws IOException {
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-	Dimension screenSize = toolkit.getScreenSize();
-	Rectangle screenRect = new Rectangle(screenSize);
-	Robot robot = null;
-	try {
-		robot = new Robot();
-	} catch (AWTException ex) {
-		System.out.println(ex);
-	}
 
-	// prise du screenshot
-	BufferedImage createScreenCapture = robot.createScreenCapture(screenRect);
-        final String NOM_FILE = "screenshot/";
-	final String NOM_IMAGE = "image"; 			// A CHANGER
-	final String EXT = "png";
-
-	// ecriture de l'image sur le disque
-	ImageIO.write(createScreenCapture, EXT, new File(NOM_FILE+NOM_IMAGE + numscreen + "." + EXT));
-        numscreen ++;
-        /*GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    public static void takeScreenshotWithAWT(final ImageView iv) throws IOException {
+        // Get all the screens available (like if dual screens)
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice[] gs = ge.getScreenDevices();
 
-        java.awt.Robot robot = null;
+        //@warning get by default the first screen!
+        int screenId = gs.length - 1;
+
+        Robot robot = null;
         try {
-            robot = new java.awt.Robot(gs[gs.length-1]);
+            robot = new Robot(gs[screenId]);
         } catch (AWTException e) {
-            System.out.println("Failed to create screenshot: "+e.getMessage());
-            System.exit(-1);
+            System.out.println("Failed to create screenshot: " + e.getMessage());
         }
-        DisplayMode mode = gs[0].getDisplayMode();
+
+        DisplayMode mode = gs[screenId].getDisplayMode();
         Rectangle bounds = new Rectangle(0, 0, mode.getWidth(), mode.getHeight());
-        final BufferedImage bi = robot.createScreenCapture(bounds);
+
+        final BufferedImage createScreenCapture = robot.createScreenCapture(bounds);
+
+        // update the javafx panel
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
-                Image im = SwingFXUtils.toFXImage(bi, null);
-                iv.setImage(im);
+                iv.setImage(SwingFXUtils.toFXImage(createScreenCapture, null));
             }
-        });*/
+        });
+
+        // store screenshot as file
+        ImageIO.write(createScreenCapture, EXT, new File(NOM_FILE + NOM_IMAGE + numscreen + "." + EXT));
+        numscreen++;
     }
-    
+
     public static void takeScreenshotWithGlass(final ImageView iv) {
         com.sun.glass.ui.Robot robot = com.sun.glass.ui.Application.GetApplication().createRobot();
         Screen mainScreen = Screen.getMainScreen();
         Pixels screenshot = robot.getScreenCapture(0, 0, mainScreen.getWidth(), mainScreen.getHeight());
         WritableImage image = new WritableImage(screenshot.getWidth(), screenshot.getHeight());
-        
+
         // to continue
-        
+
         System.out.println(screenshot.asByteBuffer());
-        System.out.println("screenshot is "+screenshot.getWidth()+" "+screenshot.getHeight());
+        System.out.println("screenshot is " + screenshot.getWidth() + " " + screenshot.getHeight());
     }
-    
+
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
      * main() serves only as fallback in case the application can not be
@@ -152,9 +128,8 @@ public class Dummy extends Application {
         /* fix for osx */
         System.setProperty("javafx.macosx.embedded", "true");
         java.awt.Toolkit.getDefaultToolkit();
-        
+
         /* start the app */
         launch(args);
     }
 }
-
